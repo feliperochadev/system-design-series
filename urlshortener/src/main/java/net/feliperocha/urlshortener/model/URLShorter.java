0de4d56e528034f8e4fdffac6c6766f7aa5b52f2
Felipe Rocha <email@feliperocha.net>
2026-08-01 17:03:49 -0400

    Modify URLShorter constructor to include shortURLIdAccept shortURLId from generator instead of self-generating UUID
    Updated constructor to accept shortURLId as a parameter.

--- urlshortener/src/main/java/net/feliperocha/urlshortener/model/URLShorter.java
+++ urlshortener/src/main/java/net/feliperocha/urlshortener/model/URLShorter.java
@@ -4,26 +4,24 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import java.util.UUID;
-
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 @Entity
-@Getter
-@RequiredArgsConstructor
-public class URLShorter {
+    @Getter
+    @RequiredArgsConstructor
+    public class URLShorter {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+@Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private Long id;
 
-    private String longURL;
-    private String shortURLId;
+private String longURL;
+        private String shortURLId;
 
-    public URLShorter(String longURL) {
-        this.longURL = longURL;
-        this.shortURLId = UUID.randomUUID().toString();
-    }
+public URLShorter(String longURL, String shortURLId) {
+    this.longURL = longURL;
+    this.shortURLId = shortURLId;
 }
+    }
